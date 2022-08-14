d303dcfae98edf63c0eb104c66f6279ba88256e1
Dima Zhadan <109739013+DimaZhadan@users.noreply.github.com>
2022-08-14 14:39:12 +0300

    Add files via upload

--- main/script.js
+++ main/script.js
@@ -1,10 +1,12 @@
 //hamburger menu
 const iconMenu = document.querySelector('.menu__icon');
+const header = document.querySelector('.header')
 if (iconMenu) {
    const menuList = document.querySelector('.header__nav');
    iconMenu.addEventListener('click', function (e) {
       document.body.classList.toggle('_lock')
       iconMenu.classList.toggle('_activete');
       menuList.classList.toggle('_activete');
+      header.classList.toggle('_activete')
    })
 }
